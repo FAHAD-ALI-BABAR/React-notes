615732271dd2fab534d346b08c3932cc824f40b2
FAHAD-ALI-BABAR <rajafahadali931@gmail.com>
2024-05-07 20:44:50 +0500

    callback

--- react-pass-generator/src/App.jsx
+++ react-pass-generator/src/App.jsx
@@ -27,10 +27,17 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-800'>
-       <h1 className='text-white text-center'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-6 py-5 my-8 text-orange-500 bg-gray-800'>
+       <h1 className='text-white text-center my-2'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type="text" className='outline-none w-full py-1 px-3' value={passwordvalid}  placeholder='Enter oassword' readOnly/>
+          <input type="text" className='outline-none w-full py-2 px-3' value={passwordvalid}  placeholder='Enter password' readOnly/>
+        </div>
+        <button className='bg-pink-200 py-2 px-6 outline-none'>Copy</button>
+        <div className='flex text-sm gap-x-2 my-6'>
+          <div className='flex items-center gap-x-1'>
+            <input type='range' min={6} max={100} value={length} onChange={(e)=>{setlength(e.target.value)}} className='cursor-pointer'/>
+            <label>Length: {length}</label>
+          </div>
         </div>
       </div>
     </>
