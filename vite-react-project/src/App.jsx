afef58f88a8875b9466e1b8ecb9bc1ebf9c5611a
FAHAD-ALI-BABAR <rajafahadali931@gmail.com>
2024-05-09 22:53:49 +0500

    useeffect

--- vite-react-project/src/App.jsx
+++ vite-react-project/src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
   const [color,setcolor]=useState("olive")
+  useEffect(()=>{
+    alert("background color has changed")
+  },[color])
   return(
     <>
     <div className="w-full h-screen duration-100" style={{backgroundColor:color}}>
