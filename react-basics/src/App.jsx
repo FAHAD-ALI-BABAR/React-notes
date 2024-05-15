343f9296603e3a1d51729e950eea7dc6a41d407b
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-05-15 21:30:38 +0500

    reactbasics

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Basic from './Basic'
+
+function App() {
+ 
+  return (
+    <>
+      <Basic/>
+    </>
+  )
+}
+
+export default App
--- whats-are-hooks/src/App.jsx
+++ whats-are-hooks/src/App.jsx
@@ -1,29 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // const [color, setcolor] = useState("")
-  const [car,setcar]=useState({
-    name:"ford",
-    color:"red",
-    model:"1996"
-  })
-  const changecolor=()=>{
-    setcar((previous)=>{
-  return {...previous, color:"yellow"}
-    })
-  }
-    
-  
- 
+  //***************************************USESTATE******************************************8 (it reflects changes in application's UI) */
+   const [color, setcolor] = useState("")
+  // const [car,setcar]=useState({
+  //   name:"ford",
+  //   color:"red",
+  //   model:"1996"
+  // })
+  // const changecolor=()=>{
+  //   setcar((previous)=>{
+  // return {...previous, color:"yellow"}
+  //   })
+  // }
+
+  //-----------------------------------------USEEFFECT-------------------------------------------(it is used to fetch data,change data when the component renders)
+
   return (
     <>
+    {/*****************************************USESTATE***************************************************** */ }
     {/* <div style={{width:200, height:100, backgroundColor:color}}></div>
     <button onClick={()=>{setcolor("blue")}} style={{backgroundColor:"blue"}}>click me</button> */}
-    <p>the color of your car is {car.color} </p>
-    <p>the new color is <button onClick={changecolor}>click</button></p>
+    {/* <p>the color of your car is {car.color} </p>
+    <p>the new color is <button onClick={changecolor}>click</button></p> */}
+
+    {/*------------------------------------------USEEFFECT---------------------------------------- */}
       </>
   )
 }
